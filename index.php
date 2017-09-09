e5732065914c6ef9f93264bc01824c35e46e88ac
Sean <romulus108@protonmail.com>
2017-09-09 18:35:18 -0400

    Updated the index page and uploaded a keybase proof.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<DOCTYPE html>
 <html>
     <head>
         <link rel='stylesheet' type='text/css' href='style.css'>
@@ -6,9 +6,18 @@
     </head>
     <body>
         <center><h3>Good morning, Dave.</h3></center>
+        <div id='sidebar'>
+        <p>
+            <a href="https://github.com/Romulus10">Romulus10 on GitHub</a>
+            <a href="https://www.python.org">Python Programming Language</a>
+            <a href="https://ruby-lang.org">Ruby Programming Language</a>
+            <a href="https://keybase.io/romulus10">Romulus10 on Keybase.io</a>
+        </p>    
         <?php
+            echo "Today's date."
             $date = date("d/m/Y");
             echo $date;
         ?>
+        </div>
     </body>
 </html>
