f6f990ef261a69b0649411533cdc36ff69b5e6e5
Sean Batzel <romulus108@protonmail.com>
2017-09-14 11:02:28 -0400

    That should work.

--- counter.py
+++ counter.py
@@ -1,12 +1,10 @@
+f = open("count.txt", "r")
 try:
-    f = open("count.txt", "r")
     x = int(f.read())
-    x += 1
-    f.close()
-    f = open("count.txt", "w")
-    f.write(x)
-    f.close()
-except Exception:
-    f = open("count.txt", "w")
-    f.write("1")
-    f.close()
+except ValueError:
+    x = 0
+x += 1
+f.close()
+f = open("count.txt", "w")
+f.write(str(x))
+f.close()
